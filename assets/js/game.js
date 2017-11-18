c6bba99122abcfc9809a9446220fec255feeb83e
CodeKhoi <30888184+CodeKhoi@users.noreply.github.com>
2017-11-18 09:55:04 -0800

    complete game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,50 +1,59 @@
 $(function() {
-
-var win = 0;
+  
+  // Global variables
+  var win = 0;
   var lose = 0;
   var counter = 0;
+  var images;
+  var random;
+  var crystal;
   var targetNumber;
+  var crystalValue;
+  var resetAndStart;
 
+  // Function to reset and start new game when player wins or losses
+  resetAndStart = function () {
 
-  var resetAndStart = function () {
-
+    // removes all content from the crystals element on restart so it doesn't duplicate crystals.
     $(".crystals").empty();
 
-    var images = [
+    images = [
           '../assets/img/gem1.jpg',
           '../assets/img/gem2.jpg',
           '../assets/img/gem3.jpg',
           '../assets/img/gem4.jpg',];
 
+    // Sets the target number to random from 19-120 and places that target number in the DOM
     targetNumber = Math.floor(Math.random() * 101 ) + 19;
 
     $("#randomNumber").html('' + targetNumber);
 
     for (var i = 0; i < 4; i++) {
+      
+      // Sets crystal values from 1-12 randomly.
+      random = Math.floor(Math.random() * 11 ) + 1;
 
-      var random = Math.floor(Math.random() * 11 ) + 1;
-
-      var crystal = $("<div>");
+        crystal = $("<div>");
           crystal.attr({
             "class": 'crystal',
             "data-random": random,
           });
-          crystal.css({
-            "background-image":"url('" + images[i] + "')",
-          })
+          crystal.css({"background-image":"url('" + images[i] + "')",})
 
       $(".crystals").append(crystal);
+    
     }
-
+    // accumalates the total score on the DOM when crystals are clicked
     $("#score").html('' + counter);
 
   }
 
 resetAndStart ();
 
+// ================== EVENT DELEGATION==========================
 $(document).on('click', ".crystal", function () {
       
-    var crystalValue = parseInt($(this).attr('data-random'));
+    crystalValue = parseInt($(this).attr('data-random'));
 
     counter += crystalValue;
 
